451bd8925993d6483b2e6f88b5f8cb66a6534701
Cassidy <62119269+systemc12ashe@users.noreply.github.com>
2024-03-16 21:14:24 -0400

    more data cleaning

--- join_3_datasets.py
+++ join_3_datasets.py
@@ -16,21 +16,3 @@ df_out = pd.read_csv('out.csv')
 # Filter rows where the year is not between 1988 and 2017
 df_out = df_out[~df_out['Acquisition Year'].astype(int).between(1988, 2017)]
 df_out.to_csv('out2.csv', index=False)
-
-  
-# merging two csv files 
-# df = pd.concat( 
-#     map(pd.read_csv, ['acq_data.csv', 'layoffs.csv', 'maang_stock_prices_by_quarter.csv'])) 
-# print(df) 
-
-
-# # merging the files 
-# joined_files = os.path.join("/", "*.csv") 
-  
-# # A list of all joined files is returned 
-# joined_list = glob.glob(joined_files) 
-  
-# # Finally, the files are joined 
-# df = pd.concat(map(pd.read_csv, joined_list), ignore_index=True) 
-# print(df)
-# df.to_csv("C:/Users/cassi/Desktop/dataMining/dataMining/out.csv", index=False)
